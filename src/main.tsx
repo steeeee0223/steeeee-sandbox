54fb280631930e8b61884fbe672bc0022e602d63
steeeee0223 <mr.steven7@gmail.com>
2023-07-02 07:33:21 +0000

    🎯  Persist user login (#1, #5)

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { Provider } from "react-redux";
 
 import "@/index.css";
 import App from "@/App";
-import store from "@/stores/store";
+import store, { persistor } from "@/stores/store";
 import { AppProvider } from "@/contexts/app";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <AppProvider>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </AppProvider>
 );
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { authReducer } from "./auth";
 import { cursorReducer } from "./cursor";
 import { directoryReducer } from "./directory";
 import { editorReducer } from "./editor";
 import { projectReducer } from "./project";
+
+const rootPersistConfig = {
+    key: "root",
+    storage,
+    /**
+     * @param whitelist
+     * @description The key names of reducers to be persisted
+     */
+    whitelist: ["auth"],
+};
 
 export const rootReducer = combineReducers({
     auth: authReducer,
@@ -16,11 +28,12 @@ export const rootReducer = combineReducers({
 });
 
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistReducer(rootPersistConfig, rootReducer),
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
 
+export const persistor = persistStore(store);
 export default store;
