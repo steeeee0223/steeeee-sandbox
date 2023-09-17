365450b200cfefd2e4538c95dc50691d110af80c
steeeee0223 <mr.steven7@gmail.com>
2023-09-17 07:44:13 +0000

    🔧 Modify resizable panel divider

--- src/components/common/PanelDivider.tsx
+++ src/components/common/PanelDivider.tsx
@@ -1,18 +1,18 @@
-import { CSSProperties } from "react";
 import { PanelResizeHandle } from "react-resizable-panels";
 
+import styles from "./PanelDivider.module.css";
 interface PanelDividerProps {
     direction: "horizontal" | "vertical";
 }
 
 export default function PanelDivider({ direction }: PanelDividerProps) {
-    const dividerStyle: CSSProperties = {
-        transitionDuration: "250ms",
-        transitionTimingFunction: "linear",
-        backgroundColor: "#bbb",
-        outline: "none",
-    };
-    const style =
-        direction === "vertical" ? { width: "1px" } : { height: "1px" };
-    return <PanelResizeHandle style={{ ...dividerStyle, ...style }} />;
+    return (
+        <PanelResizeHandle
+            className={
+                direction === "vertical"
+                    ? styles.ResizableHandleVertical
+                    : styles.ResizableHandleHorizontal
+            }
+        />
+    );
 }
