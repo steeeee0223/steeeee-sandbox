a68744c27c2bfb25a5bf0678ed250383d9bb0bb0
steeeee0223 <mr.steven7@gmail.com>
2023-10-11 14:25:50 +0000

    🧪 `test` Added tests: `@/lib/file`

--- src/lib/file.test.ts
+++ src/lib/file.test.ts
@@ -1,6 +1,14 @@
-import { getContent, getDefaultFile, getExtension } from "./file";
+import { $files, $folders, $projects } from "#/mock";
+import { DirectoryItem, Project } from "@/types";
+import {
+    getContent,
+    getDefaultFile,
+    getExtension,
+    getRefId,
+    normalizePath,
+} from "./file";
 
-describe(getExtension.name, () => {
+describe(getExtension, () => {
     it.each([
         { filename: "App.tsx", expected: "tsx" },
         { filename: "App.test.tsx", expected: "tsx" },
@@ -15,7 +23,7 @@ describe(getExtension.name, () => {
     );
 });
 
-describe(getDefaultFile.name, () => {
+describe(getDefaultFile, () => {
     it.each<{ filename: string; content?: string }>([
         { filename: "App.tsx", content: "App" },
         { filename: "App.test.tsx" },
@@ -28,4 +36,47 @@ describe(getDefaultFile.name, () => {
             expect(resContent).toEqual(content ?? "");
         }
     );
+});
+
+describe(normalizePath, () => {
+    it.each([
+        {
+            path: ["root", "components", "Button.tsx"],
+            expected: "/components/Button.tsx",
+        },
+        { path: ["root", ".gitignore"], expected: "/.gitignore" },
+    ])("Normalized path should be $expected", ({ path, expected }) => {
+        const result = normalizePath(path);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe(getRefId, () => {
+    const project = $projects.one.state;
+    const file = $files.one.state;
+    const folder = $folders.one.state;
+
+    it.each<{
+        project: Project;
+        item?: DirectoryItem;
+        expected: string;
+    }>([
+        {
+            project,
+            expected: `${project.name}-${project.projectId}`,
+        },
+        {
+            project,
+            item: file,
+            expected: `${project.name}-${project.projectId}/components/App.tsx`,
+        },
+        {
+            project,
+            item: folder,
+            expected: `${project.name}-${project.projectId}/components`,
+        },
+    ])("Ref ID should be $expected", ({ project, item, expected }) => {
+        const result = getRefId(project, item);
+        expect(result).toEqual(expected);
+    });
 });
